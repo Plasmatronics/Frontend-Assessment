418ef146277eb79ed66ccccd19d9b51f75fcedd4
Plasmatronics <garamifilms@gmail.com>
2024-07-20 03:03:28 -0400

    Cleaned up Some Files

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,6 +2,7 @@ import { extendTheme, SystemStyleObject, ThemeConfig } from "@chakra-ui/react";
 import "@fontsource/poppins";
 import "@fontsource/manrope";
 import { SHOW_SIDEBAR_WIDTH } from "./config";
+import { mode } from "@chakra-ui/theme-tools";
 
 const themeConfiguration: ThemeConfig = {
   initialColorMode: "system",
@@ -11,11 +12,16 @@ const themeConfiguration: ThemeConfig = {
 const theme = extendTheme({
   themeConfiguration,
   styles: {
-    global: {
-      "::selection": {
-        backgroundColor: "green.300", // customizing text to brand color
+    global: (props: SystemStyleObject) => ({
+      //how this dark mode is implemented is explained in the README.md
+      body: {
+        bg: mode("white", "gray.800")(props),
+        color: mode("gray.800", "whiteAlpha.900")(props),
       },
-    },
+      "::selection": {
+        backgroundColor: mode("green.300", "green.500")(props),
+      },
+    }),
   },
   fonts: {
     heading: `'manrope', sans-serif`,
@@ -29,12 +35,8 @@ const theme = extendTheme({
       300: "#DDE0E0",
       400: "#CFD1D1",
       500: "#C0C3C3",
-      600: "#adb0b0",
     },
     green: {
-      50: "#8ce99a",
-      100: "#74ff5b",
-      200: "#33e612",
       300: "#51cf66",
       400: "#58bb3f",
       500: "#1C9A0A",
@@ -52,34 +54,6 @@ const theme = extendTheme({
     sm: "0.77rem",
     md: ".9rem",
     lg: "1rem",
-    xl: "1.25rem",
-    "2xl": "1.5rem",
-    "3xl": "1.875rem",
-    "4xl": "2.25rem",
-    "5xl": "3rem",
-    "6xl": "3.75rem",
-    "7xl": "4.5rem",
-    "8xl": "6rem",
-    "9xl": "8rem",
-  },
-  fontWeights: {
-    hairline: 100,
-    thin: 200,
-    light: 300,
-    normal: 400,
-    medium: 500,
-    semibold: 600,
-    bold: 700,
-    extrabold: 800,
-    black: 1000,
-  },
-  letterSpacings: {
-    tighter: "-0.05em",
-    tight: "-0.025em",
-    normal: "0",
-    wide: "0.025em",
-    wider: "0.05em",
-    widest: "0.1em",
   },
   breakpoints: {
     base: "0em", // 0px
